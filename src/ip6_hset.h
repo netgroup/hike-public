ffa34c7cf966f24a60608e11982a70914903b1e2
Giulio Sidoretti <giuliosidoretti@gmail.com>
2021-12-23 10:33:10 +0000

    ip6_hset.h add needed includes and add functions for udp parsing

--- src/ip6_hset.h
+++ src/ip6_hset.h
@@ -9,7 +9,10 @@
 #include <linux/if_packet.h>
 #include <linux/ipv6.h>
 #include <linux/errno.h>
+#include <linux/udp.h>
 
+#include "hike_vm.h"
+#include "parse_helpers.h"
 #include "map.h"
 
 #define HIKE_IPV6_HSET_MAX		4096
@@ -33,6 +36,29 @@ struct ipv6_hset_src_key {
 
 struct ipv6_hset_dst_key {
 	struct in6_addr daddr;
+};
+
+struct ipv6_hset_nh {
+	__u8 next_header;
+};
+
+struct layer_3_4 {
+	__u8 priority:4, version:4; /* don't care about little/big endian */
+	__u8 flow_lbl[3];
+	__be16 payload_len;
+	__u8 nexthdr;
+	__u8 hop_limit;
+	struct in6_addr saddr;
+	struct in6_addr daddr;
+	/* udp */
+	__be16 source;
+	__be16 dest;
+	__be16 len;
+	__sum16 check;
+};
+
+struct udp_dst_port {
+	__u16 dst_port;
 };
 
 
@@ -92,6 +118,45 @@ ipv6_hset_dst_get_key(struct xdp_md *ctx, struct hdr_cursor *cur,
 		return -EINVAL;
 
 	key->daddr = hdr->daddr;
+
+	return 0;
+}
+
+/* hdr_cursor->nhoff must be set and must point to network header */
+static __always_inline int
+// ipv6_get_nh(struct xdp_md *ctx, struct hdr_cursor *cur)
+ipv6_get_nh(struct xdp_md *ctx, struct hdr_cursor *cur, struct ipv6_hset_nh *nh)
+
+{
+	struct ipv6hdr *hdr;
+
+	/* ctx is injected by the HIKe VM */
+	hdr = (struct ipv6hdr *)cur_header_pointer(ctx, cur, cur->nhoff, sizeof(*hdr));
+	if (unlikely(!hdr))
+		return -EINVAL;
+
+	nh->next_header = hdr->nexthdr;
+
+	return 0;
+}
+
+/* hdr_cursor->nhoff must be set and must point to network header */
+static __always_inline int
+ipv6_get_udp_port(struct xdp_md *ctx, struct hdr_cursor *cur, struct udp_dst_port *dp)
+{
+	struct layer_3_4 *layer34;
+	// struct ipv6hdr *hdr;
+
+	// hdr = (struct ipv6hdr *)cur_header_pointer(ctx, cur, cur->nhoff, sizeof(*hdr));
+	// if (unlikely(!hdr))
+	// 	return -EINVAL;
+
+	/* ctx is injected by the HIKe VM */
+	layer34 = (struct layer_3_4 *)cur_header_pointer(ctx, cur, cur->nhoff, sizeof(*layer34));
+	if (unlikely(!layer34))
+		return -EINVAL;
+
+	dp->dst_port = bpf_htons(layer34->dest);
 
 	return 0;
 }
