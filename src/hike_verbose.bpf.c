5cf9e18c837d25e21f403f9b4676f11e397cc6bb
Andrea Mayer <andrea.mayer@uniroma2.it>
2022-01-10 17:10:23 +0100

    improve trace pipe printout messages with new HIKe VM Print API
    Signed-off-by: Andrea Mayer <andrea.mayer@uniroma2.it>

--- src/hike_verbose.bpf.c
+++ src/hike_verbose.bpf.c
@@ -2,6 +2,8 @@
 
 #define HIKE_PROG_NAME hike_verbose
 
+#define HIKE_PRINT_LEVEL	7 /* DEBUG level is set by default */
+
 #include <stddef.h>
 #include <linux/in.h>
 #include <linux/if_ether.h>
@@ -40,43 +42,49 @@ HIKE_PROG(HIKE_PROG_NAME)
 	int rc;
 	int i;
 
-	DEBUG_HKPRG_PRINT("ID=0x%llx cookie=<%d>", HVM_ARG1, HVM_ARG2);
+	hike_pr_debug("ID=0x%llx cookie=<%d>", HVM_ARG1, HVM_ARG2);
 
-	if (unlikely(!info))
+	if (unlikely(!info)) {
+		hike_pr_emerg("cannot access the HIKe VM pkt_info data");
 		goto abort;
+	}
 
 	cur = pkt_info_cur(info);
 	/* no need for checking cur != NULL right here */
 
 	ip6h = (struct ipv6hdr *)cur_header_pointer(ctx, cur, cur->nhoff,
 						    sizeof(*ip6h));
-	if (unlikely(!ip6h))
+	if (unlikely(!ip6h)) {
+		hike_pr_err("cannot access the IPv6 header");
 		goto abort;
+	}
 
-	DEBUG_HKPRG_PRINT("pkt-info");
-	DEBUG_HKPRG_PRINT("dataoff=%d", cur->dataoff);
-	DEBUG_HKPRG_PRINT("nhoff=%d", cur->nhoff);
-	DEBUG_HKPRG_PRINT("thoff=%d", cur->thoff);
+	hike_pr_debug("HIKe VM Packet info");
+	hike_pr_debug("dataoff=%d", cur->dataoff);
+	hike_pr_debug("nhoff=%d", cur->nhoff);
+	hike_pr_debug("thoff=%d", cur->thoff);
 
 	udph = (struct udphdr *)cur_header_pointer(ctx, cur, cur->dataoff,
 						   sizeof(*udph));
-	if (unlikely(!udph))
+	if (unlikely(!udph)) {
+		hike_pr_err("cannot access the UDP header");
 		goto abort;
+	}
 
 	src_port = bpf_ntohs(udph->source);
 	dest_port = bpf_ntohs(udph->dest);
 	udp_len = bpf_ntohs(udph->len);
-	DEBUG_HKPRG_PRINT("udp src port=%d", src_port);
-	DEBUG_HKPRG_PRINT("udp dest port=%d", dest_port);
-	DEBUG_HKPRG_PRINT("udp len=%d", udp_len);
+	hike_pr_info("udp src port=%d", src_port);
+	hike_pr_info("udp dest port=%d", dest_port);
+	hike_pr_info("udp len=%d", udp_len);
 
 	rc = ipv6_udp_checksum(ctx, ip6h, udph, &check);
 	if (unlikely(rc)) {
-		DEBUG_HKPRG_PRINT("checksum error=%d", rc);
+		hike_pr_err("checksum error=%d", rc);
 		goto abort;
 	}
 
-	DEBUG_HKPRG_PRINT("udp check=0x%x", bpf_ntohs(check));
+	hike_pr_info("udp check=0x%x", bpf_ntohs(check));
 
 	if (unlikely(udp_len < sizeof(*udph)))
 		goto abort;
@@ -87,8 +95,10 @@ HIKE_PROG(HIKE_PROG_NAME)
 
 	/* reserve some space for storing the string to be searched */
 	pshm = hike_pcpu_shmem_obj(sizeof(struct pkt_info), struct __shm_buff);
-	if (unlikely(!pshm))
+	if (unlikely(!pshm)) {
+		hike_pr_crit("error during access to shmem");
 		goto abort;
+	}
 
 	/* set the string in the shmem, so we do not overload the stack.
 	 * In this case, the keyword to be found is pretty small and then it
@@ -116,12 +126,14 @@ HIKE_PROG(HIKE_PROG_NAME)
 	/* p points to the beginning of the UDP payload */
 	udp_poff = cur->dataoff + sizeof(*udph);
 	p = (char *)cur_header_pointer(ctx, cur, udp_poff, sizeof(*p));
-	if (unlikely(!p))
+	if (unlikely(!p)) {
 		/* since we already check for the udp_len, if we cannot access
 		 * the first byte of the payload, something very weird is just
 		 * happened...
 		 */
+		hike_pr_crit("cannot access to UDP payload");
 		goto abort;
+	}
 
 	udp_plen = udp_len - sizeof(*udph);
 	data_end = xdp_md_tail(ctx);
@@ -143,18 +155,20 @@ HIKE_PROG(HIKE_PROG_NAME)
 
 	ok = hike_pcpu_shmem_obj(sizeof(struct pkt_info) +
 				 sizeof(struct __shm_buff), __u64);
-	if (unlikely(!ok))
+	if (unlikely(!ok)) {
+		hike_pr_crit("error during access to shmem");
 		goto abort;
+	}
 
 	/* string found event stored into the shmem */
 	*ok = 1;
 
-	DEBUG_HKPRG_PRINT(">>> keyword '%s' found <<<", keyword);
+	hike_pr_notice(">>> keyword '%s' found <<<", keyword);
 out:
 	return HIKE_XDP_VM;
 
 abort:
-	DEBUG_HKPRG_PRINT("abort");
+	hike_pr_err("abort; packet is going to be dropped");
 	return XDP_ABORTED;
 #undef BUF_LEN
 }
